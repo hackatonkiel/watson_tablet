b55f61063e4f59a82d4c2c9f5885a333d383d377
developer <developer@developer-desktop.(none)>
2017-09-22 14:37:38 +0200

    TestCommit

--- src/de/bewatec/hackerthon/service/WatsonConversation.java
+++ src/de/bewatec/hackerthon/service/WatsonConversation.java
@@ -1,11 +1,22 @@
 package de.bewatec.hackerthon.service;
 
+import com.ibm.watson.developer_cloud.conversation.v1.ConversationService;
+
 public class WatsonConversation {
 
+	private static final String LOG_TAG = "Conversation";
+	private ConversationService service = null;
 	private WatsonConversationListener listener;
+
 	
 	public WatsonConversation(WatsonConversationListener listener) {
 		this.listener = listener;
 	}
 
+	
+	
+	private void init(){
+		
+	}
+	
 }
